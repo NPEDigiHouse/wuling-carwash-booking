05558aa6325159afecb3ff8378d64f8be02e85d1
rafmasloman <raflymasloman20@gmail.com>
2024-05-03 11:07:54 +0800

    feat : create utils for colors, images, and route

--- src/shared/constant/Colors.tsx
+++ src/shared/constant/Colors.tsx
@@ -1,7 +1,8 @@
 export const COLORS = {
-  primary: "#D7FE03",
-  secondary: "#83AF3B",
+  primary: "#0047D0",
+  secondary: "#3BAF5B",
   black: "#000000",
   light_gray: "#FAFAFA",
   white: "#FFFFFF",
+  danger: "#FF6161",
 };
--- src/shared/providers/RouterProvider.tsx
+++ src/shared/providers/RouterProvider.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "../../pages/Home/Home";
+import Login from "pages/Login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 

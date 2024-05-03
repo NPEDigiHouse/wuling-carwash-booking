c9a80a3bae8eb16fed2e0f8360085ae23fe6ddfa
rafmasloman <raflymasloman20@gmail.com>
2024-05-03 20:17:05 +0800

    fix : make logo into component

---
+++ src/shared/components/Navbar/NavLogo.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@mantine/core";
+
+const NavLogo = () => {
+  return (
+    <Text className="text-3xl font-bold text-primary">
+      <span className="text-black">We</span>Wash
+    </Text>
+  );
+};
+
+export default NavLogo;

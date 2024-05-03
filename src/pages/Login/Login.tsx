e38002e1ff8d0c01966af726d627659b402e4daf
rafmasloman <raflymasloman20@gmail.com>
2024-05-03 20:17:36 +0800

    feat : slicing locing page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,30 +1,81 @@
 import {
+  Box,
+  Button,
   Container,
   Flex,
+  Image,
   PasswordInput,
   SimpleGrid,
   Stack,
   Text,
   TextInput,
 } from "@mantine/core";
+import NavLogo from "shared/components/Navbar/NavLogo";
+import { COLORS } from "shared/constant/Colors";
+import { Porsche01 } from "shared/constant/Images";
 
 const Login = () => {
   return (
-    <Container>
+    <Container className="font-poppins md:px-0" size={"100%"}>
       <SimpleGrid cols={{ base: 1, lg: 2 }}>
-        <Flex direction={"row"} gap={{ base: 30, md: 50 }}>
-          <Stack>
-            <Text>Sign In</Text>
-            <Text>Hi! welcome back you have been missed</Text>
-          </Stack>
+        <Stack className="h-screen " justify="center" align="center" gap={30}>
+          <NavLogo />
+          <form className="lg:w-[400px]">
+            <Flex
+              direction={"column"}
+              gap={{ base: 30, md: 50 }}
+              className="rounded-xl border border-solid border-gray-300 px-6 py-8 shadow-lg"
+              bg={"white"}
+            >
+              <Stack align="center">
+                <Text className="text-xl font-medium">Sign In</Text>
+                <Text className="text-sm text-gray-400">
+                  Hi! welcome back you have been missed
+                </Text>
+              </Stack>
 
-          <form>
-            <Stack>
-              <TextInput label="Email" placeholder="example@gmail.com" />
-              <PasswordInput label="Password" placeholder="********" />
-            </Stack>
+              <Stack>
+                <TextInput
+                  label="Email"
+                  placeholder="example@gmail.com"
+                  classNames={{
+                    input: `bg-[#F2F2F2] h-12 rounded-lg`,
+                    label: `font-poppins mb-2.5 text-base`,
+                  }}
+                />
+                <PasswordInput
+                  label="Password"
+                  placeholder="********"
+                  classNames={{
+                    input: `bg-[#F2F2F2] h-12 rounded-lg`,
+                    label: `font-poppins mb-2.5 text-base`,
+                  }}
+                />
+              </Stack>
+
+              <Button
+                fullWidth
+                className="h-12   rounded-md bg-primary px-7 text-base font-medium drop-shadow-2xl"
+              >
+                Login
+              </Button>
+
+              <Text className="text-center text-sm text-gray-400">
+                Don’t have an account?{" "}
+                <span className="font-medium text-primary">Sign Up</span>
+              </Text>
+            </Flex>
           </form>
-        </Flex>
+        </Stack>
+
+        <Box
+          bg={COLORS.primary}
+          className="relative hidden items-center md:flex"
+        >
+          {/* <div className=""> */}
+          <Image src={Porsche01} className="absolute right-20 h-fit w-fit" />
+          {/* </div> */}
+        </Box>
       </SimpleGrid>
     </Container>
   );

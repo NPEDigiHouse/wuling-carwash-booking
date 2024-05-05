6e196ed296621597366f40da189856b20f80e02f
rafmasloman <raflymasloman20@gmail.com>
2024-05-05 13:16:00 +0800

    feat : add about hero image for about section

--- src/shared/constant/Images.tsx
+++ src/shared/constant/Images.tsx
@@ -3,5 +3,13 @@ import Porsche02 from "../../assets/images/cars/8-2-porsche-png.png";
 import WulingAlmaz from "../../assets/images/cars/383_almaz-rs-ex-pristine-white.png";
 import ContactCard from "../../assets/images/contact-us-card-bg.png";
 import HeroImage from "../../assets/images/hero background.png";
+import AboutHeroImage from "../../assets/images/hero/emile-guillemot-ci7gkM_29wA-unsplash.jpg";
 
-export { Porsche01, Porsche02, WulingAlmaz, ContactCard, HeroImage };
+export {
+  Porsche01,
+  Porsche02,
+  WulingAlmaz,
+  ContactCard,
+  HeroImage,
+  AboutHeroImage,
+};

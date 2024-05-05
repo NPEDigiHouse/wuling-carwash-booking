80921ec5faedf03209f8ebd37316b74e08f7aa68
rafmasloman <raflymasloman20@gmail.com>
2024-05-05 12:59:23 +0800

    feat : add hero image

--- src/shared/constant/Images.tsx
+++ src/shared/constant/Images.tsx
@@ -1,5 +1,7 @@
 import Porsche01 from "../../assets/images/cars/pngimg.com - porsche_PNG102847.png";
 import Porsche02 from "../../assets/images/cars/8-2-porsche-png.png";
 import WulingAlmaz from "../../assets/images/cars/383_almaz-rs-ex-pristine-white.png";
+import ContactCard from "../../assets/images/contact-us-card-bg.png";
+import HeroImage from "../../assets/images/hero background.png";
 
-export { Porsche01, Porsche02, WulingAlmaz };
+export { Porsche01, Porsche02, WulingAlmaz, ContactCard, HeroImage };

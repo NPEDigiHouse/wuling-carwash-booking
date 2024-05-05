39620783d1f122ee9a0a769cb7c593857fcf334c
rafmasloman <raflymasloman20@gmail.com>
2024-05-05 12:58:30 +0800

    fix : add third color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,13 @@ export default {
       colors: {
         primary: "#0047D0",
         secondary: "#3BAF5B",
+        third: "#FEF403",
         black: "#000000",
         light_gray: "#FAFAFA",
         text_gray: "#808080",
         white: "#FFFFFF",
         danger: "#FF6161",
+        yellow: "#FEDD00",
       },
     },
   },

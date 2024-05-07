0f6a44f21b2384cb4cb28907e37270e2c21f598a
rafmasloman <raflymasloman20@gmail.com>
2024-05-07 21:27:34 +0800

    fix : change booking router to carwash route and service route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,13 @@
-import BookingPage from "pages/Booking/Booking";
+import BookingCarwashPage from "pages/BookingCarwash/BookingCarwash";
 import HomePage from "pages/Home/Home";
 import Login from "pages/Login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { BOOKING, LOGIN, ORDER } from "shared/utils/Route";
+import {
+  BOOKING_CARSERVICE,
+  BOOKING_CARWASH,
+  LOGIN,
+  ORDER,
+} from "shared/utils/Route";
 
 const Router = () => {
   return (
@@ -10,7 +15,8 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path={LOGIN} element={<Login />} />
-        <Route path={BOOKING} element={<BookingPage />} />
+        <Route path={BOOKING_CARWASH} element={<BookingCarwashPage />} />
+        <Route path={BOOKING_CARSERVICE} element={<BookingCarwashPage />} />
         <Route path={ORDER} />
       </Routes>
     </BrowserRouter>
--- src/shared/utils/Route.ts
+++ src/shared/utils/Route.ts
@@ -1,4 +1,5 @@
 export const LOGIN = "login";
 export const REGISTER = "register";
-export const BOOKING = "booking";
+export const BOOKING_CARWASH = "booking-carwash";
+export const BOOKING_CARSERVICE = "booking-carservice";
 export const ORDER = "order";

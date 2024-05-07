3da284f40abde5bf11168b042acc367ac39a3a93
rafmasloman <raflymasloman20@gmail.com>
2024-05-08 07:45:37 +0800

    feat : add title props to booking layout

--- src/features/booking/components/BookingLayout.tsx
+++ src/features/booking/components/BookingLayout.tsx
@@ -1,9 +1,9 @@
-import { Box, Group, Image, Stack } from "@mantine/core";
+import { Box, Group, Image, Stack, Text } from "@mantine/core";
 import { IBookingLayout } from "../interfaces/BookingLayout";
 import { useLocation } from "react-router-dom";
 import CustomBreadcrumbs from "shared/components/Breadcrumbs/CustomBreadcrumbs";
 
-const BookingLayout = ({ children, thumbnail }: IBookingLayout) => {
+const BookingLayout = ({ children, thumbnail, title }: IBookingLayout) => {
   const { pathname } = useLocation();
 
   return (
@@ -18,8 +18,12 @@ const BookingLayout = ({ children, thumbnail }: IBookingLayout) => {
         />
       </Group>
 
-      <Box className="  w-fit   lg:w-[800px]">
+      <Box className="relative w-fit lg:w-[800px]">
         <Image src={thumbnail} />
+
+        <Text className="absolute -bottom-16 left-0 right-0 -z-10 mx-auto w-fit font-bold  uppercase text-gray-500 text-opacity-55 lg:text-[120px]">
+          {title}
+        </Text>
       </Box>
       {children}
     </Stack>
--- src/features/booking/interfaces/BookingLayout.ts
+++ src/features/booking/interfaces/BookingLayout.ts
@@ -3,4 +3,5 @@ import { ReactNode } from "react";
 export interface IBookingLayout {
   children: ReactNode;
   thumbnail?: string;
+  title?: string;
 }

fe363a7467705a48b0c55c67bbd0de433aaf4dcb
rafmasloman <raflymasloman20@gmail.com>
2024-05-07 22:05:28 +0800

    fix : change navbar to Navlink

--- src/shared/components/Navbar/NavbarItem.tsx
+++ src/shared/components/Navbar/NavbarItem.tsx
@@ -1,12 +1,42 @@
+import { NavLink } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 const NavbarItem = () => {
   return (
     <ul className="">
       <li className="flex gap-10">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/booking"}>Booking</Link>
-        <Link to={"/order"}>Order</Link>
+        <NavLink
+          label="Home"
+          component={Link}
+          to={"/"}
+          className="hover:bg-transparent"
+          classNames={{
+            label: `text-base`,
+          }}
+        />
+
+        <NavLink
+          label="Booking"
+          classNames={{
+            children: ``,
+            root: ` relative h-[56px] hover:bg-transparent`,
+            label: `text-base`,
+          }}
+        >
+          <NavLink label="Home" component={Link} to={"/"} />
+
+          <NavLink label="Home" component={Link} to={"/"} />
+        </NavLink>
+
+        <NavLink
+          label="Order"
+          component={Link}
+          to={"/order"}
+          className="hover:bg-transparent"
+          classNames={{
+            label: `text-base`,
+          }}
+        />
       </li>
     </ul>
   );

5873f6800bc03310b5b2601cc143fb1c1cae29ae
rafmasloman <raflymasloman20@gmail.com>
2024-05-07 21:32:50 +0800

    feat : import more cars image

--- src/shared/constant/Images.tsx
+++ src/shared/constant/Images.tsx
@@ -1,13 +1,19 @@
 import Porsche01 from "../../assets/images/cars/pngimg.com - porsche_PNG102847.png";
 import Porsche02 from "../../assets/images/cars/8-2-porsche-png.png";
+import Porsche03 from "../../assets/images/cars/Daco_5799658.png";
 import WulingAlmaz from "../../assets/images/cars/383_almaz-rs-ex-pristine-white.png";
 import ContactCard from "../../assets/images/contact-us-card-bg.png";
 import HeroImage from "../../assets/images/hero background.png";
 import AboutHeroImage from "../../assets/images/hero/emile-guillemot-ci7gkM_29wA-unsplash.jpg";
+import PorscheService from "../../assets/images/cars/Daco_4456396.png";
+import PorscheWash from "../../assets/images/cars/Daco_1608621.png";
 
 export {
   Porsche01,
   Porsche02,
+  Porsche03,
+  PorscheService,
+  PorscheWash,
   WulingAlmaz,
   ContactCard,
   HeroImage,

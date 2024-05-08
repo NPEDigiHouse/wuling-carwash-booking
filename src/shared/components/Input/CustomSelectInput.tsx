d09bd3503b8f76fc50537ebd007d958eba2c6ac2
rafmasloman <raflymasloman20@gmail.com>
2024-05-08 18:55:50 +0800

    feat : add grid size props

--- src/shared/components/Input/CustomSelectInput.tsx
+++ src/shared/components/Input/CustomSelectInput.tsx
@@ -1,14 +1,23 @@
 import { Select, SelectProps } from "@mantine/core";
 
-interface ICustomSelectInputProps extends SelectProps {}
+interface ICustomSelectInputProps extends SelectProps {
+  gridSize?: {
+    base?: number;
+    sm?: number;
+    md?: number;
+    lg?: number;
+    xl?: number;
+  };
+}
 
-const CustomSelectInput = ({ ...props }: ICustomSelectInputProps) => {
+const CustomSelectInput = ({ gridSize, ...props }: ICustomSelectInputProps) => {
   return (
     <Select
       classNames={{
         input: `bg-[#F2F2F2] h-12 rounded-full placeholder:font-poppins px-7`,
         label: `font-poppins mb-2.5 text-base font-medium`,
       }}
+      {...gridSize}
       {...props}
     />
   );
--- src/shared/components/Input/CustomTextInput.tsx
+++ src/shared/components/Input/CustomTextInput.tsx
@@ -1,14 +1,23 @@
 import { TextInput, TextInputProps } from "@mantine/core";
 
-interface ICustomTextInputProps extends TextInputProps {}
+interface ICustomTextInputProps extends TextInputProps {
+  gridSize?: {
+    base?: number;
+    sm?: number;
+    md?: number;
+    lg?: number;
+    xl?: number;
+  };
+}
 
-const CustomTextInput = ({ ...props }: ICustomTextInputProps) => {
+const CustomTextInput = ({ gridSize, ...props }: ICustomTextInputProps) => {
   return (
     <TextInput
       classNames={{
-        input: `bg-[#F2F2F2] h-12 rounded-full placeholder:font-poppins px-7`,
+        input: `bg-[#F2F2F2] h-12 rounded-full placeholder:font-poppins px-7 font-poppins`,
         label: `font-poppins mb-2.5 text-base font-medium`,
       }}
+      {...gridSize}
       {...props}
     />
   );

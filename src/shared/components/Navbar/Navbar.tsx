b82456c192ec05b21a4fa92c59a72b772fc52690
rafmasloman <raflymasloman20@gmail.com>
2024-06-12 19:27:36 +0800

    feat : using user context on navbar

--- src/shared/components/Navbar/Navbar.tsx
+++ src/shared/components/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
-import { Button, Flex, Group } from "@mantine/core";
+import { Avatar, Button, Flex, Group, Stack, Text } from "@mantine/core";
 import NavbarItem from "./NavbarItem";
 import { Link, useNavigate } from "react-router-dom";
 import NavLogo from "./NavLogo";
+import { useContext } from "react";
+import { UserRoleContext } from "context/UserRoleContext";
+import ProfileMenu from "../Menu/ProfileMenu";
 
 const Navbar = () => {
   const navigate = useNavigate();
+
+  const userRole = useContext(UserRoleContext);
 
   const handleNavigateButtonRegister = () => {
     navigate("/register");
@@ -21,20 +26,34 @@ const Navbar = () => {
       >
         <NavLogo />
         <NavbarItem />
-        <Group className="w-fit text-nowrap ">
-          <Link to={"/login"} className="">
-            Sign In
-          </Link>
-          <Button
-            classNames={{
-              root: `bg-primary h-12 w-fit px-7 rounded-full text-base font-medium`,
-              inner: ``,
-            }}
-            onClick={handleNavigateButtonRegister}
-          >
-            Sign Up
-          </Button>
-        </Group>
+        {!userRole ? (
+          <Group className="w-fit text-nowrap ">
+            <Link to={"/login"} className="">
+              Sign In
+            </Link>
+            <Button
+              classNames={{
+                root: `bg-primary h-12 w-fit px-7 rounded-full text-base font-medium`,
+                inner: ``,
+              }}
+              onClick={handleNavigateButtonRegister}
+            >
+              Sign Up
+            </Button>
+          </Group>
+        ) : (
+          <ProfileMenu
+            profileBadge={
+              <Group>
+                <Avatar size={"md"} />
+                <Stack gap={5}>
+                  <Text className="text-sm">{userRole.customer.name}</Text>
+                  <Text className="text-xs text-gray-500">{userRole.role}</Text>
+                </Stack>
+              </Group>
+            }
+          />
+        )}
       </Flex>
     </nav>
   );

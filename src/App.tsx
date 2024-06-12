074c4043d2d66787dd0149ef97eca23e051f8128
rafmasloman <raflymasloman20@gmail.com>
2024-06-12 13:56:11 +0800

    feat: setup config user provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { MantineProvider } from "@mantine/core";
 import Router from "Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import UserProvider from "context/UserRoleContext";
 // import { Provider } from "react-redux";
 // import store from "redux/Store/Store";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     // <Provider store={store}>
-    <MantineProvider>
-      <Router />
-    </MantineProvider>
-    // </Provider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <UserProvider>
+          <Router />
+        </UserProvider>
+      </MantineProvider>
+      {/* </Provider> */}
+    </QueryClientProvider>
   );
 }
 

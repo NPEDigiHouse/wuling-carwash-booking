36eac58c49d3fdc925a61acffb22345050833520
rafmasloman <raflymasloman20@gmail.com>
2024-06-13 01:12:20 +0800

    fix: add enable when token is avaiable

--- src/shared/hooks/api/Auth/useCredentialQuery.ts
+++ src/shared/hooks/api/Auth/useCredentialQuery.ts
@@ -1,10 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { AuthServiceApi } from "services";
+import TokenConfig from "shared/config/TokenConfig";
 import { CLIENT_KEY } from "shared/constant/ClientKey";
 
 export const useCredentialQuery = () => {
+  const token = TokenConfig.getToken();
+
+  console.log("token : ", token);
+
   return useQuery({
-    queryKey: [CLIENT_KEY.AUTH.GET_USER_CREDENTIAL],
+    queryKey: [CLIENT_KEY.AUTH.GET_USER_CREDENTIAL, token],
     queryFn: ({ signal }) => AuthServiceApi.credential(signal),
+    enabled: !!token,
   });
 };
--- src/shared/hooks/api/Product/useQueryAllProducts.ts
+++ src/shared/hooks/api/Product/useQueryAllProducts.ts
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { ProductServiceApi } from "services";
+import TokenConfig from "shared/config/TokenConfig";
 import { CLIENT_KEY } from "shared/constant/ClientKey";
 
 export const useQueryAllProducts = () => {
+  const token = TokenConfig.getToken();
   return useQuery({
     queryKey: [CLIENT_KEY.PRODUCT.GET_ALL_PRODUCTS],
     queryFn: ({ signal }) => ProductServiceApi.getAllProducts(signal),
+    enabled: !!token,
   });
 };

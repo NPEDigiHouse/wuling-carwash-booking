e32e43d49b2a3193c11802aa5462a6466099c1b0
rafmasloman <raflymasloman20@gmail.com>
2024-06-14 02:07:43 +0800

    feat: forget to commit like before

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,9 @@
 import AuthService from "./Auth/AuthService";
 import ProductService from "./Product/ProductService";
+import PublicService from "./Public/PublicService";
 
 const AuthServiceApi = new AuthService();
 const ProductServiceApi = new ProductService();
+const PublicServiceApi = new PublicService();
 
-export { AuthServiceApi, ProductServiceApi };
+export { AuthServiceApi, ProductServiceApi, PublicServiceApi };

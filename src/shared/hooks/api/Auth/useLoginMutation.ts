68382a996e3c541dcf84816ffb7c1caf573155b4
rafmasloman <raflymasloman20@gmail.com>
2024-06-14 23:34:57 +0800

    fix: add on error in login mutation hooks

--- src/shared/hooks/api/Auth/useLoginMutation.ts
+++ src/shared/hooks/api/Auth/useLoginMutation.ts
@@ -1,15 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import NotificationAuth from "features/Auth/components/Notifications/NotificationAuth";
 import { AuthServiceApi } from "services";
 import TokenConfig from "shared/config/TokenConfig";
 import { CLIENT_KEY } from "shared/constant/ClientKey";
 
 export const useLoginMutation = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: [CLIENT_KEY.AUTH.LOGIN_MUTATION],
     mutationFn: AuthServiceApi.login,
     onSuccess(data) {
-      TokenConfig.setToken(data.data);
+      console.log("data : ", data);
 
       if (!data.data) {
         NotificationAuth({
@@ -18,12 +19,27 @@ export const useLoginMutation = () => {
           title: "Login Gagal",
         });
       } else {
+        TokenConfig.setToken(data.data);
+
         NotificationAuth({
           message: "Login berhasil siap masuk ke akun",
           status: "SUCCESS",
           title: "Login Berhasil",
         });
+
+        queryClient.invalidateQueries({
+          queryKey: [CLIENT_KEY.AUTH.GET_USER_CREDENTIAL],
+        });
       }
+    },
+    onError() {
+      console.log("error : ");
+
+      NotificationAuth({
+        message: "Login gagal email atau password salah",
+        status: "FAILED",
+        title: "Login Gagal",
+      });
     },
   });
 };

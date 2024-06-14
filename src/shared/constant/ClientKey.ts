b058ac6a3732d80ee8647a7a812be316e6a6662d
rafmasloman <raflymasloman20@gmail.com>
2024-06-14 23:36:52 +0800

    fix: add client key create timeslot

--- src/shared/constant/ClientKey.ts
+++ src/shared/constant/ClientKey.ts
@@ -19,6 +19,7 @@ export const CLIENT_KEY = {
   TIMESLOTS: {
     GET_ALL_TIMESLOTS: "get-all-timeslots-query-id-key",
     DELETE_TIMESLOT: "delete-timeslot-mutation-id-key",
+    CREATE_TIMESLOT: "create-timeslot-mutation-id-key",
   },
   PUBLIC: {
     GET_ALL_PRODUCTS_HOMEPAGE: "get-all-products-homepage-query-id-key",

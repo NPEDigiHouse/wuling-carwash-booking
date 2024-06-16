94776a4980babe4e8dc4c4da31ebe7bf3364abee
rafmasloman <raflymasloman20@gmail.com>
2024-06-17 02:38:05 +0800

    feat : create api booking service fetch

---
+++ src/services/Booking/BookingService.ts
@@ -0,0 +1,60 @@
+/* eslint-disable no-useless-catch */
+import { ApiBaseResponse } from "shared/config/APIResponse";
+import { http } from "shared/utils/AxiosUtils";
+import {
+  IBookingRequestParams,
+  IBookingResponseParams,
+} from "./BookingServiceInterface";
+
+class BookingService {
+  async getAllBookings(
+    signal?: AbortSignal | undefined,
+  ): Promise<ApiBaseResponse<IBookingResponseParams[]>> {
+    try {
+      const response = await http.get(
+        `${import.meta.env.VITE_API_URL}/booking`,
+        { signal },
+      );
+
+      const data: ApiBaseResponse<IBookingResponseParams[]> =
+        await response.data;
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async createBookings(
+    payload: IBookingRequestParams,
+  ): Promise<ApiBaseResponse<IBookingResponseParams>> {
+    try {
+      const response = await http.post(
+        `${import.meta.env.VITE_API_URL}/booking`,
+        payload,
+      );
+
+      const data: ApiBaseResponse<IBookingResponseParams> = await response.data;
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async deleteBooking(
+    bookingId: number,
+  ): Promise<ApiBaseResponse<IBookingResponseParams>> {
+    try {
+      const response = await http.delete(
+        `${import.meta.env.VITE_API_URL}/booking/${bookingId}`,
+      );
+
+      const data: ApiBaseResponse<IBookingResponseParams> = await response.data;
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default BookingService;

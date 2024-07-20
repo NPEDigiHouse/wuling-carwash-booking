bc3f91ddec1538ca91b4e8b03f6c4f20842d5f5d
rafmasloman <raflymasloman20@gmail.com>
2024-07-20 17:29:24 +0800

    fix: add messag for promo schema

--- src/features/Admin/schema/PromoFormSchema.ts
+++ src/features/Admin/schema/PromoFormSchema.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
 
 export const PromoFormSchema = z.object({
-  promoName: z.string().min(1),
-  discount: z.number().min(1),
-  startedDate: z.coerce.date(),
-  endDate: z.coerce.date(),
+  promoName: z.string().min(1, { message: "Nama promo harus dimasukkan" }),
+  discount: z
+    .number()
+    .min(1, { message: "Diskon harus dimasukkan" })
+    .max(100, { message: "Maksimal Input Promo adalah 100" }),
+  startedDate: z.coerce.date({ message: "Tanggal dimulai harus dimasukkan" }),
+  endDate: z.coerce.date({ message: "Tanggal berakhir harus dimasukan" }),
 });
